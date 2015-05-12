8bc69677f9f48930a31b28bff5568a7bc5c3748d
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-12 02:57:01 -0500

    deploying from master

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js - node server to host www project
+
+var express = require('express'),
+	app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.set('port', process.env.port || 7000);
+
+app.get('/', function(req, res) {
+	res.sendfile('index.html', {root: __dirname + '/dist'});
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Server listening on port ' + app.get('port'));
+});
