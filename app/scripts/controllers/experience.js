d19e5bce31c249b535d89914a8c21647260c2286
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-12-28 22:19:35 -0600

    adding /experience controller and view

---
+++ app/scripts/controllers/experience.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wwwApp.controller:ExperienceCtrl
+ * @description
+ * # ExperienceCtrl
+ * Controller of the wwwApp
+ */
+angular.module('wwwApp')
+  .controller('ExperienceCtrl', function ($scope) {
+    console.log('Hello')
+  });
